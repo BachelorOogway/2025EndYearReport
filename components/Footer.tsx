b8f0270b7d1280631c73af5af5bbe0b82dcdd1f2
jiaohaonan <jiaohaonan@bytedance.com>
2025-12-22 16:31:22 +0800

    fix: 修复Footer样式、全局样式

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,25 @@
+import { Gochi_Hand } from "next/font/google";
+
+const gochiHand = Gochi_Hand({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Footer() {
   return (
     <div className="footer-wrapper">
-      <p>@TripleUni 2025</p>
+      <p
+        className={gochiHand.className}
+        style={{
+          fontStyle: "normal",
+          fontSize: "25px",
+          lineHeight: "29px",
+          color: "#93B9CE",
+          margin: 0,
+        }}
+      >
+        @TripleUni 2025
+      </p>
     </div>
   );
 }
-
